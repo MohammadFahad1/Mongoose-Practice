55e22a30735ad999a53521bfe735eb9de6590197
Office PC ABA (Fahad) <abasc1962@gmail.com>
2023-07-23 11:02:20 +0600

    Organizing the app files

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
-const express = require('express')
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import app from './app';
 
-const app = express()
-const port = 5000
+const port: number = 5000;
+
+
 
 // DB Connection
 async function bootstrap() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
         console.log("DB Connected 👌");
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
     } catch (err) {
         console.log(`Failed to connect with databse:`, err);
     }
 }
 bootstrap();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
